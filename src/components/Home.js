2bdc7d66b839de1bf8b81fb283a3602ee3c1e2a6
Plamen <pgalinov95@gmail.com>
2020-10-27 23:03:59 +0200

    first_commit

---
+++ src/components/Home.js
@@ -0,0 +1,308 @@
+import React, { useEffect, useState, } from 'react';
+import { fetchGenre, fetchMovies, fetchMovieByGenre, fetchPersons, fetchTopratedMovie, fetchByName } from '../service';
+import Carousel from "react-bootstrap/Carousel";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import { Link } from 'react-router-dom';
+import ReactStars from "react-rating-stars-component";
+import notavailable from "../../src/notavailable.jpg";
+
+
+ function Home() {
+    const [nowPlaying, setNowPlaying] = useState([]);
+    const [isBusy, setisBusy] = useState(true);
+    const [genres, setGenres] = useState([]);
+    const [moviesByGenre, setmoviesByGenre] = useState([]);
+    const [persons, setPersons] = useState([]);
+    const [topRated, setToprated] = useState([]);
+    const [moviesByName, setMoviesByName] = useState([]);
+    const [name, setName] = useState();
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setNowPlaying(await fetchMovies());
+            setGenres(await fetchGenre());  
+            setmoviesByGenre(await fetchMovieByGenre(28));
+            setPersons(await fetchPersons());
+            setToprated(await fetchTopratedMovie());
+            setisBusy(false);
+        };
+      
+        fetchAPI();
+    }, []);
+
+    const randomIndex = (max) => {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    let startIndex = randomIndex(15);
+    let endIndex = startIndex + 5;
+
+    const handleGenreClick = async (genre_id) => {
+        setmoviesByGenre(await fetchMovieByGenre(genre_id));
+    }
+
+    const handleInput = e => {
+        let nameInput=e.target.value;
+        setName(prevState => {
+            return {...prevState, name: nameInput};
+          });
+    }
+
+    const  search = async (e) => {
+        if(e.key === "Enter") {
+         setMoviesByName(await fetchByName(e.target.value));
+        }
+      }
+
+   const posterMovies = nowPlaying.slice(startIndex, endIndex).map((item) =>  item);
+
+   const genreList = genres.map((item, index) => {
+    return (
+        <li className="list-inline-item" key={index}>
+            <button type="button" className="btn btn-outline-info" onClick={() => {
+                handleGenreClick(item.id)
+            }}>
+            {item.name}
+            </button>
+        </li>
+    )
+   })
+
+   let searchResults;
+   if(moviesByName) {
+       searchResults = moviesByName.slice(0, 4).filter((item) => !item.poster.includes("null")).map((item, index) => {
+        return (
+            
+            <div className="col-md-3 col-sm-6" key={index}>     
+                <div className="card">
+               
+                    <Link to={`/movie/${item.id}`}>
+                   <img className="img-fluid mx-auto d-block"  src={item.poster} />
+                    </Link>
+                </div>
+                <div className="mt-2">
+                    <p style={{fontWeight: 'bolder'}}>{item.title}</p>
+                    <p>Rated: {item.rating}</p>
+                    <ReactStars
+                        count={item.rating} 
+                        size={20} 
+                        color={'#f4c10f'}
+                    />
+                </div>
+            </div> 
+        )
+    })
+   }
+
+   const movieList = moviesByGenre.slice(0, 4).map((item, index) => {
+       return (
+           <div className="col-md-3 col-sm-6" key={index}>
+               <div className="card">
+                    <Link to={`/movie/${item.id}`}>
+                    <img className="img-fluid" src={item.poster} alt={item.title}></img>
+                    </Link>
+               </div>
+                <div className="mt-2">
+                    <p style={{fontWeight: 'bolder'}}>{item.title}</p>
+                    <p>Rated: {item.rating}</p>
+                    <ReactStars
+                        count={item.rating} 
+                        size={20} 
+                        color={'#f4c10f'}
+                    />
+                </div>
+           </div>
+       )
+   })
+
+   const trendingPersons = persons.slice(0, 4).map((p, i) => {
+    return(
+        <div className="col-md-3" key={i}>
+            <img className="img-fluid rounded-circle mx-auto d-block" src={p.profileImg} alt={p.name} />
+        <p className="font-weight-bold text-center mt-2">{p.name}</p>
+        </div>
+    )
+   })
+
+   const topRatedList = topRated.slice(0, 4).map((p, i) => {
+       return (
+           <div className="col-md-3" key={i}>
+               <div className="card">
+                    <Link to={`/movie/${p.id}`}>
+                        <img className="img-fluid" src={p.poster} alt={p.title}></img>
+                    </Link>
+               </div>
+               <div className="mt-2">
+                    <p style={{fontWeight: 'bolder'}}>{p.title}</p>
+                    <p>Rated: {p.rating}</p>
+                    <ReactStars
+                        count={p.rating} 
+                        size={20} 
+                        color={'#f4c10f'}
+                    />
+                </div>
+           </div>
+       )
+   })
+
+    return ( 
+        <>
+          {isBusy ? (
+              <div>Loading..</div>
+          ) : (
+          <div className="container">
+          <div className="row">
+              <div className="col">
+              <Carousel>
+                <Carousel.Item> 
+                    <Link to={`/movie/${posterMovies[0].id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <img
+                    className="d-block w-100"
+                    src={posterMovies[0].backPoster}
+                    alt="First slide"
+                    />    
+                    </Link>
+                    <Carousel.Caption style={{background: "rgba(0, 0, 0, 0.7)", borderRadius: "25px", padding: "10px"}}>
+                     <h3 >{posterMovies[0].title}</h3>
+                    <p >{posterMovies[0].overview}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                     <Link to={`/movie/${posterMovies[0].id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <img
+                    className="d-block w-100"
+                    src={posterMovies[4].backPoster}
+                    alt="Third slide"
+                    />
+                     </Link>
+                    <Carousel.Caption style={{background: "rgba(0, 0, 0, 0.7)", borderRadius: "25px", padding: "10px" }}>
+                    <h3>{posterMovies[4].title}</h3>
+                    <p >{posterMovies[4].overview}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Link to={`/movie/${posterMovies[0].id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <img
+                    className="d-block w-100"
+                    src={posterMovies[3].backPoster}
+                    alt="Third slide"
+                    />
+                    </Link>
+                    <Carousel.Caption style={{background: "rgba(0, 0, 0, 0.7)", borderRadius: "25px", padding: "10px" }}>
+                    <h3>{posterMovies[3].title}</h3>
+                    <p>{posterMovies[3].overview}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                </Carousel>
+              </div>
+          </div>
+
+          <div className="searchbox-wrap">
+            <input type="text" 
+            placeholder="Search for a movie..." 
+            className="searchbox"
+            name="search" 
+            required
+            onChange={handleInput}
+            onKeyPress={search} />
+          </div>
+
+           <div className="row mt-3">
+                <div className="col">
+                    <ul className="list-inline">
+                        {genreList}
+                    </ul>
+                </div>   
+            </div> 
+ 
+            
+            {searchResults && searchResults.length > 0 ? (<div className="font-weight-bold" style={{color: '#5a606b'}}>Search results:</div>) : ""}
+            <div className="row mt-3"> {searchResults} </div>
+
+            <div className="row mt-3">
+                <div className="col">
+                    <div className="float-right">
+                        <i className="fa fa-arrow-alt-circle-right"></i>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="row mt-3">
+                {movieList}
+            </div>
+
+            <div className="row mt-3">
+                <div className="col">
+                    <p className="font-weight-bold" style={{color: '#5a606b'}}>
+                        TRENDING ACTORS ON THIS WEEK
+                    </p>
+                </div>
+            </div>
+
+            <div className="row mt-3">
+                <div className="col">
+                    <div className="float-right">
+                        <i className="fa fa-arrow-alt-circle-right"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row mt-3">
+                {trendingPersons}
+            </div>
+
+            <div className="row mt-3">
+                <div className="col">
+                    <p className="font-weight-bold" style={{color: '#5a606b'}}>TOP RATED MOVIES</p>
+                </div>
+            </div>
+
+            <div className="row mt-3">
+                <div className="col">
+                    <div className="float-right">
+                        <i className="fa fa-arrow-alt-circle-right"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row mt-3">
+                {topRatedList}
+            </div>
+
+            <hr className="mt-5" style={{borderTop: "1px solid #5a606b"}}></hr>
+
+            <div className="row mt-3 mb-5">
+                <div className="col-md-8 col-sm-6" style={{color:"#5a606b"}}>
+                    <h3>ABOUT ME</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
+                    <ul className="list-inline">
+                        <li className="list-inline-item">
+                           <a href="/" style={{color: "#f4c10f"}}>
+                               <i className="fab fa-github"></i>
+                           </a>
+                        </li>
+                    </ul>
+                </div>  
+                <div className="col-md-4 col-sm-6" style={{color:"#5a606b"}}>
+                  <h3>KEEP IN TOUCH</h3>
+                    <ul className="list-unstyled">
+                        <li>
+                            <p>
+                                <i className="fas fa-map-marker-alt"></i> Address: Plovdiv, Bulgaria
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+          
+
+          </div> 
+          )}
+       </>
+    )
+    
+}
+
+export default Home;
---
+++ src/components/MovieDetails.js
@@ -0,0 +1,242 @@
+import React, { useEffect, useState } from 'react';
+import { fetchMovieDetail, fetchMovieVideos, fetchMovieCasts, fetchSimilarMovie } from '../service';
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import { Modal } from 'react-bootstrap';
+import ReactPlayer from 'react-player';
+import ReactStars from 'react-rating-stars-component';
+import {Link} from "react-router-dom";
+import avatar from "../../src/notavailable.jpg";
+
+
+function MovieDeatils({match}) {
+    let params = match.params;
+    let genres = []
+    const [detail, setDetail] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [video, setVideo] = useState([]);
+    const [isBusy, setisBusy] = useState(true);
+    const [casts, setCasts] = useState([]);
+    const [similarMovies, setSimilarMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDetail(await fetchMovieDetail(params.id));
+            setVideo(await fetchMovieVideos(params.id));
+            setCasts(await fetchMovieCasts(params.id));
+            setSimilarMovies(await fetchSimilarMovie(params.id));
+            setisBusy(false);
+        };
+
+        fetchAPI();
+    }, [params.id])
+
+    genres = detail.genres;
+
+    const MoviePlayerModal = (props) => {
+        const youtubeUrl = 'https://www.youtube.com/watch?v=';
+        return (
+           <Modal
+           {...props}
+           size="lg"
+           aria-labelledby="contained-modal-title-vcenter"
+           centered
+           >
+            <Modal.Header closeButton>
+                 <Modal.Title
+                 id="contained-modal-title-vcenter"
+                 style={{color: 'black', fontWeight: 'bolder'}}
+                 >
+                {detail.title}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body style={{backgroundColor: '00000'}}>
+                <ReactPlayer
+                className="container-fluid"
+                url={youtubeUrl + video.key}
+                playing
+                width="100%"
+                controls="true"
+                >
+                </ReactPlayer>
+            </Modal.Body>
+           </Modal> 
+        )
+    }
+
+ 
+
+    let genreList;
+    if(genres){
+        genreList = genres.map((g, i) => {
+            return (
+                <li className="list-inline-item" key={i}>
+                    <button type="button" className="btn btn-outline-info">
+                        {g.name}
+                    </button>
+                </li>
+            );
+        });
+    }
+    
+    const castList = casts.slice(0, 4).map((c, i) => {
+        console.log(casts);
+        return (
+            <div className="col-md-3" key={i}>
+                {c.img.includes("w200null") ? <img className="img-fluid rounded-circle mx-auto d-block" src={avatar} /> :  <img className="img-fluid rounded-circle mx-auto d-block"  src={c.img} />}
+                <p className="font-weight-bold text-center mt-2">{c.name}</p>
+                <p className="font-weight-light text-center" style={{color: "white"}}>{c.character}</p>
+            </div>
+        )
+    })
+
+    const similarMoviesList = similarMovies.slice(0, 4).map((c, i) => {
+        return (
+            <div className="col-md-3 col-sm-6" key={i}>
+            <div className="card">
+                 <Link to={`/movie/${c.id}`}>
+                     <img className="img-fluid" src={c.poster} alt={c.title}></img>
+                 </Link>
+            </div>
+             <div className="mt-2">
+                 <p style={{fontWeight: 'bolder'}}>{c.title}</p>
+                 <p>Rated: {c.rating}</p>
+                 <ReactStars
+                     count={c.rating} 
+                     size={20} 
+                     color={'#f4c10f'}
+                 />
+             </div>
+        </div>
+        )
+    })
+
+    return(
+        <>
+         {isBusy ? (
+              <div>Loading..</div>
+          ) : (
+        <div className="container">
+            <div className="row mt-2">
+                <MoviePlayerModal
+                show={isOpen}
+                onHide={() => {
+                    setIsOpen(false);
+                }}
+                >
+                </MoviePlayerModal>
+                <div className="col text-center" style={{width: '100%'}}>
+                    <img className="img-fluid" src={`https://image.tmdb.org/t/p/original/${detail.backdrop_path}`} alt={detail.title} />
+                        <div className="carousel-center">
+                            <i 
+                            className="far fa-play-circle" 
+                            style={{fontSize: 95, color: '#f4c10f', cursor: 'pointer'}} 
+                            onClick={() => setIsOpen(true)}>
+                            </i>
+                        </div>   
+                        <div className="carousel-caption" >
+                            <h2>{detail.title}</h2>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+
+            <div className="row mt-3">
+                <div className="col">
+                    <p style={{color: '#5a606b', fontWeight: "bolder"}}>GENRE</p>
+                </div>
+            </div>
+
+            <div className="row mt-3">
+                <div className="col">
+                    <ul className="list-inline">
+                        {genreList}
+                    </ul>
+                </div>
+            </div>
+
+            <div className="row mt-3">
+                <div className="col">
+                    <div className="text-center">
+                        <ReactStars
+                        count={detail.vote_average}
+                        size={20}
+                        color={'#f4c10f'}
+                        >
+                        </ReactStars>
+                    </div>
+                <div className="mt-3">
+                    <p style={{color: '#5a606b', fontWeight: "bolder"}}>OVERVIEW</p>
+                    {detail.overview}
+                </div>
+                </div>
+            </div>
+
+            <div className="row mt-3">
+                <div className="col-md-3">
+                    <p style={{color: "#5a606b", fontWeight: 'bolder'}}>RELEASE DATE</p>
+                    <p style={{color: "#f4c10f"}}>{detail.release_date}</p>
+                </div>
+                <div className="col-md-3">
+                    <p style={{color: "#5a606b", fontWeight: 'bolder'}}>RUN TIME</p>
+                    <p style={{color: "#f4c10f"}}>{detail.runtime} minutes</p>
+                </div>
+                <div className="col-md-3">
+                    <p style={{color: "#5a606b", fontWeight: 'bolder'}}>BUDGET</p>
+                    {detail.budget > 0 ? (<p style={{color: "#f4c10f"}}>$ {detail.budget}</p>) : (<p style={{color: "#f4c10f"}}>N/A</p>)}
+                    
+                </div>
+            </div>
+
+            <div className="row mt-3">
+                <div className="col">
+                    <p style={{color: '#5a606b', fontWeight: "bolder"}}>CASTS</p>
+                </div>
+            </div>
+            <div className="row mt-3">
+                    {castList}
+            </div>
+
+            <div className="row mt-3">
+                <div className="col">
+                    <p style={{color: '#5a606b', fontWeight: "bolder"}}>SIMILAR MOVIES</p>
+                </div>
+            </div>
+            <div className="row mt-3">
+                {similarMoviesList}
+            </div>
+
+            <hr className="mt-5" style={{borderTop: "1px solid #5a606b"}}></hr>
+
+            <div className="row mt-3 mb-5">
+                <div className="col-md-8 col-sm-6" style={{color:"#5a606b"}}>
+                    <h3>ABOUT ME</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
+                    <ul className="list-inline">
+                        <li className="list-inline-item">
+                           <a href="/" style={{color: "#f4c10f"}}>
+                               <i className="fab fa-github"></i>
+                           </a>
+                        </li>
+                    </ul>
+                </div>  
+                <div className="col-md-4 col-sm-6" style={{color:"#5a606b"}}>
+                  <h3>KEEP IN TOUCH</h3>
+                    <ul className="list-unstyled">
+                        <li>
+                            <p>
+                                <i className="fas fa-map-marker-alt"></i> Address: Plovdiv, Bulgaria
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
+        </div>
+        )}
+    </>
+    );
+}
+
+export default MovieDeatils;
